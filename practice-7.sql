e54e6784067352886156f8d9a059e035f9e730e5
parasmidha <paras.midha@kellogg.northwestern.edu>
2022-04-15 03:34:39 +0000

    completed practice-7

--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,8 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
-
+select listings.property_type, count(*), reviews.date_reviewed 
+from listings inner join reviews on listings.id = reviews.listing_id
+group by listings.property_type
+order by max (reviews.date_reviewed)
+;
